0d414475896adfc0309dedd1081d9c7af117568e
Joshua Ibrom <joshuaibrom@gmail.com>
2023-01-08 09:36:52 -0600

    Tentative home page completed.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,16 @@
 import { useQuery, gql } from "@apollo/client";
-import { createStyles, Group, Text, Title } from "@mantine/core";
+import {
+  createStyles,
+  Group,
+  MediaQuery,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
 import CountUp from "react-countup";
+import SpaceXBg from "../assets/img/spacex_bg.jpg";
 
+/** Defines the query to gather the needed information about the company. */
 const GET_CEO = gql`
   {
     company {
@@ -13,16 +22,50 @@ const GET_CEO = gql`
   }
 `;
 
-const useStyles = createStyles((_) => ({
+/** Defines the set of style classes that may be applied to the components
+ * below.
+ */
+const useStyles = createStyles((theme) => ({
+  page: {
+    display: "flex",
+    flexDirection: "column",
+    flexGrow: 1,
+    backgroundImage: `url(${SpaceXBg})`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+  },
+
   hero: {
     display: "flex",
     flexDirection: "column",
     flexGrow: 1,
     alignItems: "center",
     justifyContent: "center",
+    gap: `${theme.spacing.md}px`,
+    backgroundColor: `${theme.colors.dark[7]}BF`,
+    textAlign: "center",
+    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+      alignItems: "start",
+      textAlign: "left",
+    },
+  },
+
+  title: {
+    fontSize: "4rem",
+  },
+
+  highlight: {
+    fontWeight: "bold",
+    fontSize: `${theme.fontSizes.xl}px`,
+  },
+
+  credit: {
+    backgroundColor: `${theme.colors.dark[7]}BF`,
   },
 }));
 
+/** Defines a formatter that may be used to format currency, in USD. */
 const formatter = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
@@ -31,36 +74,86 @@ const formatter = new Intl.NumberFormat("en-US", {
 export const HomePage = () => {
   const { classes } = useStyles();
   const { loading, error, data } = useQuery(GET_CEO);
-  if (loading) return <div>Loading</div>;
-  if (error) return <div>Error: {error.message}</div>;
-
-  const formatCurrency = (n: number): string => {
-    return formatter.format(n);
-  };
+  if (loading) return <div>Loading</div>; // TODO: Better loading message.
+  if (error) return <div>Error: {error.message}</div>; // TODO: Prettier error message.
 
   return (
-    <div className={classes.hero}>
-      <Title>SpaceX Lookup</Title>
-      <Group>
-        <Text>
-          <CountUp start={0} end={data.company.launch_sites} duration={3} />{" "}
-          Active Launch Sites
-        </Text>
-        <Text>
-          <CountUp start={0} end={data.company.employees} duration={3} />{" "}
-          Employees
-        </Text>
-        <Text>
-          <CountUp
-            start={0}
-            end={data.company.valuation}
-            duration={3}
-            formattingFn={formatCurrency}
-          />{" "}
-          Valuation
-        </Text>
-      </Group>
-      <Text>{data.company.summary}</Text>
+    <div className={classes.page}>
+      <div className={classes.hero}>
+        <Title className={classes.title}>SpaceX Lookup</Title>
+        <MediaQuery smallerThan="sm" styles={{ display: "none" }}>
+          <Group>
+            <Text>
+              <CountUp
+                start={0}
+                end={data.company.launch_sites}
+                duration={3}
+                className={classes.highlight}
+              />{" "}
+              Active Launch Sites
+            </Text>
+            <Text>
+              <CountUp
+                start={0}
+                end={data.company.employees}
+                duration={3}
+                className={classes.highlight}
+              />{" "}
+              Employees
+            </Text>
+            <Text>
+              <CountUp
+                start={0}
+                end={data.company.valuation}
+                duration={3}
+                formattingFn={formatter.format}
+                className={classes.highlight}
+              />{" "}
+              Valuation
+            </Text>
+          </Group>
+        </MediaQuery>
+        <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+          <Stack>
+            <Text>
+              <CountUp
+                start={0}
+                end={data.company.launch_sites}
+                duration={3}
+                className={classes.highlight}
+              />{" "}
+              Active Launch Sites
+            </Text>
+            <Text>
+              <CountUp
+                start={0}
+                end={data.company.employees}
+                duration={3}
+                className={classes.highlight}
+              />{" "}
+              Employees
+            </Text>
+            <Text>
+              <CountUp
+                start={0}
+                end={data.company.valuation}
+                duration={3}
+                formattingFn={formatter.format}
+                className={classes.highlight}
+              />{" "}
+              Valuation
+            </Text>
+          </Stack>
+        </MediaQuery>
+        <Text>{data.company.summary}</Text>
+      </div>
+      <Text ta="center" size="xs" className={classes.credit}>
+        Photo credit,{" "}
+        <a href="https://www.flickr.com/photos/spacex/52475966140/">
+          SpaceX on Flickr
+        </a>
+        .
+      </Text>
     </div>
   );
 };

aea7af565b68f9b02ff9b222be3584716dceadda
Joshua Ibrom <joshuaibrom@gmail.com>
2023-01-09 10:58:00 -0600

    Built out ship info page

--- src/pages/ShipById.tsx
+++ src/pages/ShipById.tsx
@@ -1,5 +1,15 @@
 import { useQuery, gql, DocumentNode } from "@apollo/client";
-import { createStyles, Alert } from "@mantine/core";
+import {
+  createStyles,
+  Alert,
+  Space,
+  Title,
+  Container,
+  Card,
+  Badge,
+  Grid,
+  List,
+} from "@mantine/core";
 import { IconAlertCircle } from "@tabler/icons";
 import { useLoaderData } from "react-router-dom";
 
@@ -14,17 +24,35 @@ export const loader = ({ params }: any) => {
       type
       year_built
       status
+      successful_landings
+      roles
+      attempted_landings
     }
   }
   `;
 };
 
-const useStyles = createStyles((_) => ({
+const useStyles = createStyles((theme) => ({
   loadingErrorDiv: {
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
     flexGrow: 1,
+  },
+
+  cardImage: {
+    width: "100%",
+    height: "auto",
+  },
+
+  cardTitle: {
+    display: "flex",
+    flexDirection: "column",
+    [`@media (min-width: ${theme.breakpoints.sm}px)`]: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+    },
   },
 }));
 
@@ -48,5 +76,64 @@ export const ShipPage = () => {
     );
 
   const ship = data.ships[0];
-  return <div>{ship.name}</div>;
+  return (
+    <main>
+      <Space h="lg" />
+      <Container>
+        <Card>
+          {ship.image && (
+            <Card.Section>
+              <img
+                src={ship.image}
+                alt="Image of the ship."
+                className={classes.cardImage}
+              />
+              <Space h="md" />
+            </Card.Section>
+          )}
+          <div className={classes.cardTitle}>
+            <Title>
+              {ship.name} ({ship.year_built})
+            </Title>
+            <Badge>{ship.type}</Badge>
+          </div>
+          <Grid>
+            {ship.roles.map((role: string) => (
+              <Grid.Col key={role}>
+                <Badge variant="outline">{role}</Badge>
+              </Grid.Col>
+            ))}
+          </Grid>
+          <List>
+            {ship.status && (
+              <List.Item>
+                <strong>Status:</strong> {ship.status}
+              </List.Item>
+            )}
+            {ship.url && (
+              <List.Item>
+                <strong>More Info:</strong>{" "}
+                <a href={ship.url} target="_blank" referrerPolicy="no-referrer">
+                  {ship.url}
+                </a>
+              </List.Item>
+            )}
+            <List.Item>
+              <strong>Home Port:</strong> {ship.home_port}
+            </List.Item>
+            {ship.attempted_landings && (
+              <List.Item>
+                <strong>Attempted Landings:</strong> {ship.attempted_landings}
+              </List.Item>
+            )}
+            {ship.successful_landings && (
+              <List.Item>
+                <strong>Successful Landings:</strong> {ship.successful_landings}
+              </List.Item>
+            )}
+          </List>
+        </Card>
+      </Container>
+    </main>
+  );
 };

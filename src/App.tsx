fe059741f0b98e7f810ceeed4adbd943ce3ee5ac
Joshua Ibrom <joshuaibrom@gmail.com>
2023-01-08 18:02:06 -0600

    Installed react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Layout } from "./pages/_Layout";
 import { HomePage } from "./pages/Home";
 
+const toRouteElement = (children: JSX.Element): JSX.Element => (
+  <Layout>{children}</Layout>
+);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: toRouteElement(<HomePage />),
+  },
+  {
+    path: "/search",
+    element: toRouteElement(<div>In dev.</div>),
+  },
+]);
+
 export default function App() {
-  return (
-    <Layout>
-      <HomePage />
-    </Layout>
-  );
+  return <RouterProvider router={router} />;
 }

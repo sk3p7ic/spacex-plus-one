2b85714363cb0bd9e3323ee62d09df8d94f56f7a
Joshua Ibrom <joshuaibrom@gmail.com>
2023-01-09 10:34:37 -0600

    Styled error and loading states

--- src/pages/ShipById.tsx
+++ src/pages/ShipById.tsx
@@ -1,4 +1,6 @@
 import { useQuery, gql, DocumentNode } from "@apollo/client";
+import { createStyles, Alert } from "@mantine/core";
+import { IconAlertCircle } from "@tabler/icons";
 import { useLoaderData } from "react-router-dom";
 
 export const loader = ({ params }: any) => {
@@ -17,11 +19,33 @@ export const loader = ({ params }: any) => {
   `;
 };
 
+const useStyles = createStyles((_) => ({
+  loadingErrorDiv: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexGrow: 1,
+  },
+}));
+
 export const ShipPage = () => {
   const { loading, error, data } = useQuery(useLoaderData() as DocumentNode);
-  if (loading) return <div>Loading...</div>;
+  const { classes } = useStyles();
+  if (loading) return <div className={classes.loadingErrorDiv}>Loading...</div>;
   if (error || data.ships.length === 0)
-    return <div>Error: {error?.message}</div>;
+    return (
+      <div className={classes.loadingErrorDiv}>
+        <Alert
+          icon={<IconAlertCircle size={18} />}
+          title="Something went wrong!"
+          color="red"
+          withCloseButton
+          closeButtonLabel="Close this message."
+        >
+          {error?.message}
+        </Alert>
+      </div>
+    );
 
   const ship = data.ships[0];
   return <div>{ship.name}</div>;
